a8c9cad3b5fe57295574ccf55bf301d4adf2762e
AndrewLorets <andrewlorets@gmail.com>
2024-08-04 04:36:58 +0300

    update

--- Assets/_Workspace/Scripts/Door.cs
+++ Assets/_Workspace/Scripts/Door.cs
@@ -5,56 +5,52 @@ public class Door : MonoBehaviour
     [SerializeField] private Sprite _interactSprite;
     [SerializeField] private Sprite _playerIcon;
     [SerializeField] private CharacterDialogue _dialogue;
-    [SerializeField] private bool isOpen = false;
+    [SerializeField] private bool _isOpen;
 
     private Animation _animation;
     private Collider _collider;
 
     private const string ANIM_OPEN = "Door_Open";
     private const string ANIM_CLOSE = "Door_Close";
+
     private void Awake()
     {
         _animation = GetComponent<Animation>();
         _collider = GetComponent<Collider>();
     }
+    private void Start()
+    {
+        if (_isOpen)
+            OpenDoor(); 
+    }
     private void OnTriggerEnter(Collider other)
     {
-        if (!other.CompareTag(TagsObj.PLAYER)) return;
-        if (!isOpen)
+        if (!_isOpen && other.CompareTag(TagsObj.PLAYER))
             InteractionManager.Interact(InputKey, true, _interactSprite);
-        else OpenDoor(); 
     }
     private void OnTriggerExit(Collider other)
     {
-        if (!other.CompareTag(TagsObj.PLAYER)) return;
-        if (!isOpen)
+        if (!_isOpen && other.CompareTag(TagsObj.PLAYER))
             InteractionManager.Interact(InputKey, false, _interactSprite);
-        else
-            CloseDoor();
     }
-
     private void InputKey()
     {
         foreach (var t in Inventory.items)
         {
             if(t.itemName == "Door Key")
             {
-                isOpen = true;
+                _isOpen = true;
+                _collider.enabled = false; 
                 OpenDoor(); 
                 InteractionManager.Interact(InputKey, false, _interactSprite);
             }
         }
-        if (!isOpen)
+        if (!_isOpen)
             CharacterMessanger.instance.SetDialogueMessage(_playerIcon, _dialogue.text, _dialogue.clip);
     }
     private void OpenDoor()
     {
-        _animation.Play(ANIM_OPEN);
+        _animation.PlayQueued(ANIM_OPEN);
         AudioManager.Instance.PlayOpenDoor(); 
-    }
-    private void CloseDoor()
-    {
-        _animation.Play(ANIM_CLOSE);
-        AudioManager.Instance.PlayOpenDoor();
     }
 }
