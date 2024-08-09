5ec28ebaf0f91310e6a690e397df3c6d9f7d59fe
AndrewLorets <andrewlorets@gmail.com>
2024-08-09 17:37:24 +0300

    update

--- Assets/_Workspace/Scripts/AudioManager.cs
+++ Assets/_Workspace/Scripts/AudioManager.cs
@@ -1,4 +1,5 @@
 using DG.Tweening;
+using System.Collections;
 using UnityEngine;
 
 public class AudioManager : MonoBehaviour
@@ -6,6 +7,7 @@ public class AudioManager : MonoBehaviour
     [Header("Music")]
     [SerializeField] private AudioSource _musicSource;
     [Space(5), SerializeField] private AudioClip _musicGameClip;
+    [SerializeField] private AudioClip _musicAlertClip;
     [SerializeField] private AudioClip _musicWinClip;
     [SerializeField] private AudioClip _musicLoseClip; 
     [Header("Fx")]
@@ -14,55 +16,45 @@ public class AudioManager : MonoBehaviour
     [SerializeField] private AudioSource _item;
     [SerializeField] private AudioSource _alert;
 
+    private EnemyManager _enemyManager;
+    private bool _isAlert;
     private float _startMusicVolume; 
     private void Awake()
     {
         ServiceLocator.RegisterService(this);
 
-        GameManager.onGameStart += OnGameStart;
-        GameManager.onGameWin += OnGameWin;
-        GameManager.onGameLose += OnGameLose;
+        GameManager.onGameStart += PlayIdleMusic;
+        GameManager.onGameWin += PlayWinMusic;
+        GameManager.onGameLose += PlayLoseMusic;
+        GameManager.onGameStart += StartCheckAlertState; 
     }
     private void OnDestroy()
     {
-        GameManager.onGameStart -= OnGameStart;
-        GameManager.onGameWin -= OnGameWin;
-        GameManager.onGameLose -= OnGameLose;
+        GameManager.onGameStart -= PlayIdleMusic;
+        GameManager.onGameWin -= PlayWinMusic;
+        GameManager.onGameLose -= PlayLoseMusic;
+        GameManager.onGameStart -= StartCheckAlertState;
     }
     private void Start()
     {
+        _enemyManager = ServiceLocator.GetService<EnemyManager>();
         _startMusicVolume = _musicSource.volume; 
     }
-    private void OnGameStart()
+    private void StartCheckAlertState() => StartCoroutine(CheckAlertState());
+    private void PlayIdleMusic() => ChangeMusic(_musicGameClip);
+    private void PlayWinMusic() => ChangeMusic(_musicWinClip);
+    private void PlayLoseMusic() => ChangeMusic(_musicLoseClip);
+    private void ChangeMusic(AudioClip clip)
     {
         _musicSource.DOFade(0, .5f)
             .OnComplete(delegate ()
             {
-                _musicSource.Stop();
-                _musicSource.volume = _startMusicVolume;
-                _musicSource.PlayOneShot(_musicGameClip);
+                _musicSource.clip = clip;
+                _musicSource.Play(); 
+                _musicSource.DOFade(_startMusicVolume, .15f);
             });
     }
-    private void OnGameWin()
-    {
-        _musicSource.DOFade(0, .5f)
-            .OnComplete(delegate ()
-            {
-                _musicSource.Stop();
-                _musicSource.volume = _startMusicVolume;
-                _musicSource.PlayOneShot(_musicWinClip);
-            });
-    }
-    private void OnGameLose()
-    {
-        _musicSource.DOFade(0, .5f)
-            .OnComplete(delegate ()
-            {
-                _musicSource.Stop();
-                _musicSource.volume = _startMusicVolume; 
-                _musicSource.PlayOneShot(_musicLoseClip);
-            });       
-    }
+
     public void PlayOpenDoor()
     {
         _door.Play(); 
@@ -80,4 +72,42 @@ public class AudioManager : MonoBehaviour
         if (!_alert.isPlaying)
             _alert.Play(); 
     }
+
+    private IEnumerator CheckAlertState()
+    {
+        WaitForSeconds waitForSeconds = new WaitForSeconds(3); 
+
+        while (GameManager.gameState == GameManager.GameState.GamePlay)
+        {
+            bool shouldPlayAlertMusic = false;
+
+            if (_enemyManager.enemiesList.Count > 0)
+            {
+                foreach (var enemy in _enemyManager.enemiesList)
+                {
+                    if (enemy.onAttack)
+                    {
+                        shouldPlayAlertMusic = true;
+                        break;
+                    }
+                }
+            }
+
+            if (shouldPlayAlertMusic != _isAlert)
+            {
+                _isAlert = shouldPlayAlertMusic;
+                if (_isAlert)
+                {
+                    ChangeMusic(_musicAlertClip);
+                }
+                else
+                {
+                    ChangeMusic(_musicGameClip); 
+                }
+            }
+
+            yield return waitForSeconds;
+        }
+    }
+
 }
